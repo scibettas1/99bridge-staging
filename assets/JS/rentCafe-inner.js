08878046dda06f43eb4bf4575232f884b3d7579d
Samantha Scibetta <reception@Millenium.local>
2021-07-07 16:28:18 -0400

    finally got the directions to render

--- assets/JS/rentCafe-inner.js
+++ assets/JS/rentCafe-inner.js
@@ -3,21 +3,26 @@ $(document).ajaxComplete(function () {
   $('title').text(function (index, text) { return text.replace('99 Bridge | Custom Page', 'Features & Amenities of 99 Bridge'); });
 });
 
+// replace resident login with contact
+$('#login-links').append('<li id="contact-btn"><a href="contactus.aspx">CONTACT</a></li>');
+
 // added divs to the slide in menu
 
 $('#menu').prepend('<div class="ninety9"><img src="https://scibettas1.github.io/99bridge-staging/assets/images/99graphic.png"></div>');
 
-$('#menu').prepend('<div class="menu-logo"><img src="https://scibettas1.github.io/99bridge-staging/assets/images/logobw.png"></div>');
+$('#menu #HomeMenu').prepend('<div class="menu-logo"><img src="https://scibettas1.github.io/99bridge-staging/assets/images/logobw.png"></div>');
 
 // added divs to the navbar
 
 $('#landing').prepend('<div id="custom-nav">This is Where the specials should go</div>');
 
+$('#info_panel').append('<section><ul class="socialmedia-icons"><li class="social-media-Facebook "><a data-selenium-id="SM_FaceBookIcon" class="facebook-icon" target="_blank" aria-label="Facebook" href="https://www.facebook.com/99BridgeLuxuryRentals/"></a></li><li class="social-media-Instagram "><a data-selenium-id="SM_InstagramIcon" class="instagram-icon" target="_blank" aria-label="Instagram" href="https://www.instagram.com/bne_realestate/"></a></li></section>');
+
 // change the navbar as you scroll
 
 $(document).on('scroll', function () {
     var img = $("#landing #logo-container img")
-    if ($(this).scrollTop() >= $('#content').position().top) {
+    if ($(this).scrollTop() >= $('#content').position().top - 75) {
       img.attr("src", img.attr("src").replace('https://cdngeneralcf.rentcafe.com/dmslivecafe/2/70799/logobw.png?&quality=85', 'https://cdngeneralcf.rentcafe.com/dmslivecafe/2/70799/logo-color.png'));
     } else {
       img.attr("src", img.attr("src").replace('https://cdngeneralcf.rentcafe.com/dmslivecafe/2/70799/logo-color.png', 'https://cdngeneralcf.rentcafe.com/dmslivecafe/2/70799/logobw.png?&quality=85'));
@@ -25,15 +30,7 @@ $(document).on('scroll', function () {
     console.log("this is running");
   });
 
-  const currentYear = new Date().getFullYear();
-
-console.log(currentYear)
-
-$('#text').text(function (index, text) { return text.replace('" ©' + currentYear + 'BNE Real Estate Group.  All Rights Reserved.&nbsp;|&nbsp;Website Design by RENT"<i>Café</i>', '©' + currentYear + 'BNE Real Estate Group.  All Rights Reserved. | Website Design by Sam Scibetta'); });
-$(document).ajaxComplete(function () {
-    $('#text').text(function (index, text) { return text.replace('" ©' + currentYear + 'BNE Real Estate Group.  All Rights Reserved.&nbsp;|&nbsp;Website Design by RENT"<i>Café</i>', '©' + currentYear + 'BNE Real Estate Group.  All Rights Reserved. | Website Design by Sam Scibetta'); });
-}
-);
+  $('#footer-image').prepend('<div id="new-address">(732) 970-8444 &nbsp|&nbsp <a href="mailto:info@rent99bridge.com">info@Rent99Bridge.com</a><br /><a href="https://www.google.com/maps/place/99+Old+Amboy+Rd,+Old+Bridge,+NJ+08857/@40.37934,-74.310975,17z/data=!3m1!4b1!4m5!3m4!1s0x89c3cde3ce6de4a5:0xd3102c555754178c!8m2!3d40.37934!4d-74.3087863" target="_blank">99 Old Amboy Road, Old Bridge, NJ 08857</a></div>');
 
 $('#eho-copyright').append('<div class="">Web Design by <a href="https://scibettas1.github.io/#/" target="_blank">Sam Scibetta</a></div>');
 
--- assets/JS/rentcafe.js
+++ assets/JS/rentcafe.js
@@ -1,8 +1,11 @@
+// replace resident login with contact
+$('#login-links').append('<li id="contact-btn"><a href="contactus.aspx">CONTACT</a></li>');
+
 // added divs to the slide in menu
 
 $('#menu').prepend('<div class="ninety9"><img src="https://scibettas1.github.io/99bridge-staging/assets/images/99graphic.png"></div>');
 
-$('#menu').prepend('<div class="menu-logo"><img src="https://scibettas1.github.io/99bridge-staging/assets/images/logobw.png"></div>');
+$('#menu #HomeMenu').prepend('<div class="menu-logo"><img src="https://scibettas1.github.io/99bridge-staging/assets/images/logobw.png"></div>');
 
 // added divs to the navbar
 
@@ -10,19 +13,23 @@ $('#landing').prepend('<div id="custom-nav">This is Where the specials should go
 
 // tagline
 
-$('#landing').append('<div id="tagline" class="tagline">Your Life.<br/>Your Way.<div class="sub-tag">New 1 & 2 Bedroom<br/>Luxury Rentals Now Leasing</div><div><p class="scroll">DISCOVER</p><p class="arrow">&#8595;<p></div></div>');
+$('#landing').append('<div id="tagline" class="tagline">Your Life. Your Way.<div class="sub-tag">New 1 & 2 Bedroom<br/>Luxury Rentals Now Leasing</div></div><div id="caret"><p class="arrow">&#94;</p></div>');
 
 // change the navbar as you scroll
 
 $(document).on('scroll', function () {
   var img = $("#landing #logo-container img")
   // if ($(window).scrollTop() >= 1000 && $(window).scrollTop() < 1850) {
-    if ($(this).scrollTop() >= $('#about').position().top && $(this).scrollTop() <= $('#gallery').position().top) {
+    if ($(this).scrollTop() >= $('#about').position().top - 100 && $(this).scrollTop() <= $('#gallery').position().top - 100) {
     img.attr("src", img.attr("src").replace('https://cdngeneralcf.rentcafe.com/dmslivecafe/2/70799/logobw.png?&quality=85', 'https://cdngeneralcf.rentcafe.com/dmslivecafe/2/70799/logo-color.png'));
+    console.log("first switch");
+  } else 
+  if ($(this).scrollTop() >= $('#gallery-image').position().top - 100 && $(this).scrollTop() <= $('#amenities').position().top - 100) {
+    img.attr("src", img.attr("src").replace('https://cdngeneralcf.rentcafe.com/dmslivecafe/2/70799/logobw.png?&quality=85', 'https://cdngeneralcf.rentcafe.com/dmslivecafe/2/70799/logo-color.png'));
+    console.log("second switch");
   } else {
     img.attr("src", img.attr("src").replace('https://cdngeneralcf.rentcafe.com/dmslivecafe/2/70799/logo-color.png', 'https://cdngeneralcf.rentcafe.com/dmslivecafe/2/70799/logobw.png?&quality=85'));
   }
-  console.log("this is running");
 });
 
 
@@ -33,17 +40,27 @@ $(document).ajaxComplete(function () {
   $('#ctmcontentcontainer h1').text(function (index, text) { return text.replace('99 Bridge', 'AN ADDRESS OF SOPHISTICATION'); });
 });
 
-// add gellery button
+// add availability button
 
-$('#gallery-caption').append('<br /><a href="photogallery.aspx" class="btn btn btn-primary" id="gallerybtn" aria-label="View Photo Gallery">View Gallery</a>');
+$('#gallery-caption').append('<br /><div class="floorplans"><a href="floorplans.aspx" class="btn btn btn-primary" id="gallerybtn">Availability</a></div');
+
+// add text over gallery image
+
+$('#gallery #gallery-image .overlay').prepend('<div class="amenities"><h1>AMENITIES THAT AMAZE</h1><div class="normaltext">Customize your ideal lifestyle through an exciting array of community amenities, deicated to delivering the perfect live/work/play balance. Abundant indoor and outdoor spaces include social and co-working lounges, state-of-the-art fitness facilities, a kids playroom, pet spa, a sun terrace with an outdoor bar, BBQs, fire pit, a swimming pool, and so much more. With so many carefully considered amenities, getting away from it all is just as easy as steppingoutside your door.<br /><br /><br /><a class="btn btn btn-primary" href="photogallery.aspx"><span style="font-size: 21px">View Gallery</span></a></div></div></div>');
 
 $('#gallery #gallery-image .overlay').prepend('<div class="ninety9"><img src="https://scibettas1.github.io/99bridge-staging/assets/images/99graphic.png"></div>');
 
-$('#com-amenities-title').append('<div class="neighborhood-sub-title">HIGHER EDUCATION AND<br />THE GREAT OUTDOORS</div>');
+//add caption to location section
+
+// $('#com-amenities-title').append('<div class="neighborhood-sub-title">HIGHER EDUCATION AND<br />THE GREAT OUTDOORS</div>');
 
 //add map to the bottom of the page
 
 $('#amenities').append('<div id="map"><iframe width="100%" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCGr0yjNEz31lMcJkV0_sF2rJ2WR57GXdI&q=place_id:ChIJpeRtzuPNw4kRjBdUV1UsENM" allowfullscreen></iframe></div>');
+
+
+$('#footer-image').prepend('<div id="new-address">(732) 970-8444 &nbsp|&nbsp <a href="mailto:info@rent99bridge.com">info@Rent99Bridge.com</a><br /><a href="https://www.google.com/maps/place/99+Old+Amboy+Rd,+Old+Bridge,+NJ+08857/@40.37934,-74.310975,17z/data=!3m1!4b1!4m5!3m4!1s0x89c3cde3ce6de4a5:0xd3102c555754178c!8m2!3d40.37934!4d-74.3087863" target="_blank">99 Old Amboy Road, Old Bridge, NJ 08857</a></div>');
+
 
 function initMap() {
   const myLatLng = { lat: 40.37934, lng: -74.310975 };
